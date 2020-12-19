4740dd0a50c602aa3c5d76ed54bee5887709037f
Evelyn Fredin <evefre2001@skola.goteborg.se>
2020-12-20 00:46:35 +0100

    Setting up the header

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: "media",
+  theme: {
+    extend: {
+      colors: {
+        accent: "#2DCAAD",
+        primary: {
+          100: "#DFDEE7",
+          200: "#C4B9DA",
+          300: "#847DB4",
+          400: "#092137"
+        }
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
