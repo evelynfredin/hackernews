6de7f875be3b7eebbf2e89e72eda115660e40e83
Evelyn Fredin <evefre2001@skola.goteborg.se>
2020-12-19 23:46:35 +0100

    Tailwind is now part of the project

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,9 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hacker News</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body>
+<body class="bg-blue-500">
+
+
     @yield('content')
 </body>
 
