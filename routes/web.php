60cc5dec767061b39dce1fd7e358ae99a9361d74
Evelyn Fredin <evefre2001@skola.goteborg.se>
2020-12-22 00:10:32 +0100

    Fixing failed check

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\UserController;
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
 
 Route::get('/register', [UserController::class, 'index'])->name('register');
 
