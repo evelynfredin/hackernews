7e0e2578ac4c6cf4f7f66528a517e24f48c3888a
Evelyn Fredin <evefre2001@skola.goteborg.se>
2020-12-25 13:32:56 +0100

    Users can now logout and be redirected to homepage afterwards

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\Auth\UserController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 
 Route::get('/register', [UserController::class, 'index'])->name('register');
 Route::post('/register', [UserController::class, 'store']);
@@ -11,8 +12,10 @@ Route::post('/register', [UserController::class, 'store']);
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+
 Route::get('/settings', [SettingsController::class, 'index'])->name('settings');
 
 Route::get('/', function () {
     return view('news.index');
-});
+})->name('home');
